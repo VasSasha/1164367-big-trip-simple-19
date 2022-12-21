bf37b42a9ac44b189e55748037db546e05823504
Alexandra <doccaa44@gmail.com>
2022-12-21 12:45:32 +0700

    add blocks render

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
+import Filters from './view/filters-view';
+import TripEvents from './view/trip-events-view';
+import TripEventsSorting from './view/trip-events-sorting-view';
 
+import { render } from './render.js';
+
+// const siteMainElement = document.querySelector('.main');
+// const siteHeaderElement = document.querySelector('.main__control');
+const siteTripsBlock = document.querySelector('.trip-events');
+const siteFiltersBlock = document.querySelector('.trip-controls__filters')
+
+render(new Filters(), siteFiltersBlock);
+render(new TripEventsSorting(), siteTripsBlock);
+render(new TripEvents(), siteTripsBlock);
